cc4b28913ec8de4748b9f4342dac03e6b15dbb81
Tutuka Za <x2qxqbh77w29h03h@gmail.com>
2015-12-05 01:06:39 +0300

    some thinking and rethinking

--- lib/Ajika/main.rb
+++ lib/Ajika/main.rb
@@ -1,12 +1,21 @@
 require_relative 'base'
 
-module Ajika
-  class Application < Base
+if self.to_s == "main"
+  # We are probably at the top level.
+
+  puts "We are probably at the top level."
+
+  require 'forwardable'
+  self.extend Forwardable
+  @ajika_app = Class.new(Ajika::Application)
+  self.def_delegators :@ajika_app, *Ajika::Application::DSL.instance_methods
+
+  at_exit do
+    # Don't run @angelo_app on uncaught exceptions including exit
+    # being called which raises SystemExit.  The rationale being that
+    # exit means exit, not "run the server".
+    if $!.nil?
+      @ajika_app.run(STDIN.read)
+    end
   end
-
-  at_exit { puts "at_exit" }
 end
-
-# include would include the module in Object
-# extend only extends the `main` object
-extend Ajika::Delegator
--- test2.rb
+++ test2.rb
@@ -2,7 +2,21 @@
 
 require_relative 'lib/Ajika'
 
-from 'post@blogger.com' do
+category 'blog' do
+	if_from 'from@me.com'
+	if_to   'you@name.com'
+	if_subj ''
+	if_key  'key'
 
+	# db_to 'path'
+	# render_to 'path'
 end
 
+# reply '' do
+# 	subj /^(RE|re)(\[\S+\])?\:(.+)$/
+
+# 	db_to path
+# 	render_to path
+# end
+
+puts self.class
